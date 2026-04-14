f925c6c5dc1c305f722e5a93ea7f86206a4ef1d8
Yangix-w <l.shi1@campus.unimib.it>
2026-04-14 20:11:06 +0200

    Modified script.sh

--- matlab/script.sh
+++ matlab/script.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-DIR="./matrices"
-mkdir -p $DIR
-curl -L -o $DIR/Flan_1565.mat https://suitesparse-collection-website.herokuapp.com/mat/Janna/Flan_1565.mat
 
-curl -L -o $DIR/StocF-1465.mat https://suitesparse-collection-website.herokuapp.com/mat/Janna/StocF-1465.mat
+urls=(
+  "https://suitesparse-collection-website.herokuapp.com/mat/Janna/Flan_1565.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/Janna/StocF-1465.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/Rothberg/cfd2.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/Rothberg/cfd1.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/AMD/G3_circuit.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/Wissgott/parabolic_fem.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/GHS_psdef/apache2.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/MaxPlanck/shallow_water1.mat"
+    "https://suitesparse-collection-website.herokuapp.com/mat/FIDAP/ex15.mat"
+)
 
-curl -L -o $DIR/cfd2.mat https://suitesparse-collection-website.herokuapp.com/mat/Rothberg/cfd2.mat
+dest="./matrices"
 
-curl -L -o $DIR/cfd1.mat https://suitesparse-collection-website.herokuapp.com/mat/Rothberg/cfd1.mat
+# Crea la cartella (senza errore se esiste)
+mkdir -p "$dest"
 
-curl -L -o $DIR/G3_circuit.mat https://suitesparse-collection-website.herokuapp.com/mat/AMD/G3_circuit.mat
+for url in "${urls[@]}"; do
+  file=$(basename "$url")
+  path="$dest/$file"
 
-curl -L -o $DIR/parabolic_fem.mat https://suitesparse-collection-website.herokuapp.com/mat/Wissgott/parabolic_fem.mat
+  echo "Scarico $file..."
 
-curl -L -o $DIR/apache2.mat https://suitesparse-collection-website.herokuapp.com/mat/GHS_psdef/apache2.mat
-
-curl -L -o $DIR/shallow_water1.mat https://suitesparse-collection-website.herokuapp.com/mat/MaxPlanck/shallow_water1.mat
-
-curl -L -o $DIR/ex15.mat https://suitesparse-collection-website.herokuapp.com/mat/FIDAP/ex15.mat
+  # Download
+  curl -L -o "$path" "$url"
+  echo
+done
